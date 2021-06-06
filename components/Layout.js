12c0981afaf3ca9631e480a3b14a5a30298779fc
justin40715 <56301721+justin40715@users.noreply.github.com>
2021-06-06 22:27:22 +0800

    fourth commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,11 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import Header from './Header';
+import Showcase from '@/components/Showcase';
 import Footer from './Footer';
 import styles from '@/styles/Layout.module.css';
 export default function Layout({ title, description, keyword, children }) {
+  const router = useRouter();
   return (
     <div>
       <Head>
@@ -10,6 +13,7 @@ export default function Layout({ title, description, keyword, children }) {
         <meta name={keyword} content={description} />
       </Head>
       <Header />
+      {router.pathname === '/' && <Showcase />}
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
