e4045f2c5675b2d35a00955ba8809d8d28d1436a
justin40715 <56301721+justin40715@users.noreply.github.com>
2021-06-06 23:18:16 +0800

    seventh commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 import Layout from '@/components/Layout';
-export default function HomePage() {
+import { API_URL } from '@/config/index';
+export default function HomePage({ events }) {
+  console.log('HomePage', events);
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  console.log('getStaticProps', events);
+  return {
+    props: {
+      events,
+    },
+    revalidate: 1,
+  };
 }
