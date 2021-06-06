883f5e56fa2cbe549f805874cec671bcd7667181
justin40715 <56301721+justin40715@users.noreply.github.com>
2021-06-06 23:27:22 +0800

    ninth commit

--- pages/account/login.js
+++ pages/account/login.js
@@ -1,7 +1,51 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useState, useEffect, useContext } from 'react';
+import Link from 'next/link';
+import { FaUser } from 'react-icons/fa';
+import Layout from '@/components/Layout';
+import styles from '@/styles/AuthForm.module.css';
 export default function LoginPage() {
-    return (
-        <div>
-            Login
-        </div>
-    )
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('email password', email, password);
+};
+  return (
+    <Layout title='User Login'>
+      <div className={styles.auth}>
+        <h1>
+          <FaUser /> Log In
+        </h1>
+        <ToastContainer />
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='email'>Email Address</label>
+            <input
+              type='email'
+              id='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor='email'>Password</label>
+            <input
+              type='password'
+              id='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <input type='submit' value='Login' className='btn' />
+        </form>
+        <p>
+          Don't have an account?
+          <Link href='/account/register'>Register</Link>
+        </p>
+      </div>
+    </Layout>
+  );
 }
+
